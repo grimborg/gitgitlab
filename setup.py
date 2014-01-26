d1b7d0e2db759576219ca1e0bee4242c953889b4
Òscar Vilaplana <dev@oscarvilaplana.cat>
2014-01-26 22:54:33 +0100

    requirements on setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     version='1.0',
     description='gitlab for git',
     packages=['gitgitlab'],
-    install_requires=map(str.strip, open('requirements.txt', 'r').readlines()),
+    install_requires=['gitpython==0.3.2.RC1', 'keyring==3.3', 'opster==4.1', 'python-gitlab==0.6'],
     entry_points={'console_scripts': ['git-lab=gitgitlab.cli:git']}
 )
 
